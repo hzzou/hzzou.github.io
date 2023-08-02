d906554a2ef4029d42934a871fdc5e55e1b892ea
hzlzh(邹兴鸿) <987565275@qq.com>
2023-08-03 00:04:40 +0800

    优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL} from "node:url";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 // antd的cssinjs自带自动导入
 // import vitePluginImp from "vite-plugin-imp";
 
